6e903f8bfc72f4838ece0056148a00dadd1a6d82
gokul34-65 <gokulkarunaharan41@gmail.com>
2025-10-05 17:17:31 +0530

    functional improvements

--- src/main/java/com/saanya/quiz_app/config/CorsConfig.java
+++ src/main/java/com/saanya/quiz_app/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // React app URL
+                .allowedOrigins("http://localhost:5173") // React app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
